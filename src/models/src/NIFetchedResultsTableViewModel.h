948b580081d8dfb4c13d14587e2c1d45253938f4
Francis Li <mail@francisli.com>
2012-11-07 19:28:56 -0800

    Moved common NSFetchedResultsController table implementation into the model

--- src/models/src/NIFetchedResultsTableViewModel.h
+++ src/models/src/NIFetchedResultsTableViewModel.h
@@ -8,11 +8,38 @@
 
 #import <Foundation/Foundation.h>
 
-@interface NIFetchedResultsTableViewModel : NITableViewModel {
+@class NIFetchedResultsTableViewModel;
+
+@protocol NIFetchedResultsTableViewModelDelegate <NSObject, NITableViewModelDelegate>
+
+@optional
+
+- (UITableViewRowAnimation)tableViewModel:(NIFetchedResultsTableViewModel *)tableViewModel
+                   rowAnimationForSection:(id <NSFetchedResultsSectionInfo>)sectionInfo
+                                  atIndex:(NSUInteger)sectionIndex
+                            forChangeType:(NSFetchedResultsChangeType)type
+                           withController:(NSFetchedResultsController *)controller
+                              inTableView:(UITableView *)tableView;
+
+- (UITableViewRowAnimation)tableViewModel:(NIFetchedResultsTableViewModel *)tableViewModel
+                    rowAnimationForObject:(id)anObject
+                              atIndexPath:(NSIndexPath *)indexPath
+                            forChangeType:(NSFetchedResultsChangeType)type
+                             newIndexPath:(NSIndexPath *)newIndexPath
+                           withController:(NSFetchedResultsController *)controller
+                              inTableView:(UITableView *)tableView;
+
+@end
+
+@interface NIFetchedResultsTableViewModel : NITableViewModel <NSFetchedResultsControllerDelegate> {
     @private
     NSFetchedResultsController *_controller;
+    __weak UITableView *_tableView;
 }
+@property (nonatomic, NI_WEAK) id<NIFetchedResultsTableViewModelDelegate> delegate;
 
-- (id)initWithFetchedResultsController:(NSFetchedResultsController *)controller delegate:(id<NITableViewModelDelegate>)delegate;
+- (id)initWithFetchedResultsController:(NSFetchedResultsController *)controller
+                             tableView:(UITableView *)tableView
+                              delegate:(id<NIFetchedResultsTableViewModelDelegate>)delegate;
 
 @end
--- src/models/src/NIFetchedResultsTableViewModel.m
+++ src/models/src/NIFetchedResultsTableViewModel.m
@@ -10,11 +10,15 @@
 
 @implementation NIFetchedResultsTableViewModel
 
-- (id)initWithFetchedResultsController:(NSFetchedResultsController *)controller delegate:(id<NITableViewModelDelegate>)delegate
+- (id)initWithFetchedResultsController:(NSFetchedResultsController *)controller
+                             tableView:(UITableView *)tableView
+                              delegate:(id<NIFetchedResultsTableViewModelDelegate>)delegate
 {
     self = [super initWithDelegate:delegate];
     if (self) {
         _controller = controller;
+        _controller.delegate = self;
+        _tableView = tableView;
     }
     return self;
 }
@@ -41,12 +45,88 @@
 
 - (NSArray *)sectionIndexTitlesForTableView:(UITableView *)tableView
 {
-    return nil;//_controller.sectionIndexTitles;
+    if (self.sectionIndexType != NITableViewModelSectionIndexNone) {
+        return _controller.sectionIndexTitles;
+    }
+    return nil;
 }
 
 - (NSInteger)tableView:(UITableView *)tableView sectionForSectionIndexTitle:(NSString *)title atIndex:(NSInteger)index
 {
     return [_controller sectionForSectionIndexTitle:title atIndex:index];
+}
+
+- (void)controllerWillChangeContent:(NSFetchedResultsController *)controller {
+    [_tableView beginUpdates];
+}
+
+
+- (void)controller:(NSFetchedResultsController *)controller didChangeSection:(id <NSFetchedResultsSectionInfo>)sectionInfo
+           atIndex:(NSUInteger)sectionIndex forChangeType:(NSFetchedResultsChangeType)type {
+    
+    UITableViewRowAnimation animation = UITableViewRowAnimationAutomatic;
+    if ([self.delegate respondsToSelector:@selector(tableViewModel:rowAnimationForSection:atIndex:forChangeType:withController:inTableView:)]) {
+        animation = [self.delegate tableViewModel:self
+                           rowAnimationForSection:sectionInfo
+                                          atIndex:sectionIndex
+                                    forChangeType:type
+                                   withController:_controller
+                                      inTableView:_tableView];
+    }
+    switch(type) {
+        case NSFetchedResultsChangeInsert:
+            [_tableView insertSections:[NSIndexSet indexSetWithIndex:sectionIndex]
+                      withRowAnimation:animation];
+            break;
+            
+        case NSFetchedResultsChangeDelete:
+            [_tableView deleteSections:[NSIndexSet indexSetWithIndex:sectionIndex]
+                      withRowAnimation:animation];
+            break;
+    }
+}
+
+
+- (void)controller:(NSFetchedResultsController *)controller didChangeObject:(id)anObject
+       atIndexPath:(NSIndexPath *)indexPath forChangeType:(NSFetchedResultsChangeType)type
+      newIndexPath:(NSIndexPath *)newIndexPath {
+    
+    UITableViewRowAnimation animation = UITableViewRowAnimationAutomatic;
+    if ((type != NSFetchedResultsChangeMove) && [self.delegate respondsToSelector:@selector(tableViewModel:rowAnimationForObject:atIndexPath:forChangeType:newIndexPath:withController:inTableView:)]) {
+        animation = [self.delegate tableViewModel:self
+                            rowAnimationForObject:anObject
+                                      atIndexPath:indexPath
+                                    forChangeType:type
+                                     newIndexPath:newIndexPath
+                                   withController:_controller
+                                      inTableView:_tableView];
+    }
+    switch(type) {
+            
+        case NSFetchedResultsChangeInsert:
+            [_tableView insertRowsAtIndexPaths:[NSArray arrayWithObject:newIndexPath]
+                              withRowAnimation:animation];
+            break;
+            
+        case NSFetchedResultsChangeDelete:
+            [_tableView deleteRowsAtIndexPaths:[NSArray arrayWithObject:indexPath]
+                              withRowAnimation:animation];
+            break;
+            
+        case NSFetchedResultsChangeUpdate:
+            [_tableView reloadRowsAtIndexPaths:[NSArray arrayWithObject:indexPath]
+                              withRowAnimation:animation];
+            break;
+            
+        case NSFetchedResultsChangeMove:
+            [_tableView moveRowAtIndexPath:indexPath toIndexPath:newIndexPath];
+            break;
+    }
+}
+
+
+- (void)controllerDidChangeContent:(NSFetchedResultsController *)controller {
+    [_tableView endUpdates];
 }
 
 @end
